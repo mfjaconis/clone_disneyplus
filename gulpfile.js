13c007512a4aaa4abe86efe8dae50531546b06c0
mfjaconis <matheusjaconis@gmail.com>
2024-08-29 15:49:02 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const imagemin = require("gulp-imagemin");
+const uglify = require("gulp-uglify");
 
 function styles() {
 	return gulp
@@ -16,8 +17,16 @@ function images() {
 		.pipe(gulp.dest("./dist/images"));
 }
 
-exports.default = gulp.parallel(styles, images);
+function scripts() {
+	return gulp
+		.src("./src/scripts/*.js")
+		.pipe(uglify())
+		.pipe(gulp.dest("./dist/js"));
+}
+
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = () => {
 	gulp.watch("./src/styles/*.scss", gulp.parallel(styles));
+	gulp.watch("./src/scrpits/*.js", gulp.parallel(scripts));
 };
